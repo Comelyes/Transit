d9b2ae9b74284bf64bf9fba7f031f834b7777750
comelyes <stas@baryshnikov.online>
2022-12-14 17:58:26 +0800

    TestController

---
+++ Controllers/TestController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Transit.Controllers;
+
+public class TestController : Controller
+{
+    [HttpPost]
+    [Route("/api/test")]
+    public async Task<string> Post(string data = "default")
+    {
+        return $"{data}";
+    }
+    [HttpGet]
+    [Route("/api/test")]
+    public async Task<string> Get(string data = "default")
+    {
+        return $"{data}";
+    }
+    [HttpPut]
+    [Route("/api/test")]
+    public async Task<string> Put(string data = "default")
+    {
+        return $"{data}";
+    }
+    [HttpPost]
+    [Route("/api/testjson")]
+    public async Task<string> Json(string json)
+    {
+        Console.WriteLine(json);
+        return json;
+    }
+}
